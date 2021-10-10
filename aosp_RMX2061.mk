203a461184ca447e66855cbef9829642f52dd6bd
Debdeep Chatterjee <80145521+debdeep199X@users.noreply.github.com>
2021-10-10 11:12:55 +0530

    PE bring-up

--- aosp_RMX2061.mk
+++ aosp_RMX2061.mk
@@ -10,12 +10,11 @@ $(call inherit-product, device/realme/RMX2061/device.mk)
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-WITH_CORE_GAPPS := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
-EXTENDED_BUILD_TYPE := OFFICIAL
-
-# Inherit some common AospExtended stuff
-$(call inherit-product, vendor/aosp/common.mk)
+# Inherit some common AOSP stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier
 PRODUCT_NAME := aosp_RMX2061
@@ -24,7 +23,7 @@ PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
 PRODUCT_MANUFACTURER := Realme
 
-PRODUCT_GMS_CLIENTID_BASE := android-realme
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="atoll-user 11 RKQ1.200903.002/ eng.root.04080114.013112 release-keys"
