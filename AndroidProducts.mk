e3db8fb2c809afb1fe347078d3a125e25025f2ec
Debdeep <80145521+debdeep199X@users.noreply.github.com>
2021-10-17 09:26:07 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/arrow_RMX2061.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_miatoll-user \
-    arrow_miatoll-userdebug \
-    arrow_miatoll-eng
+    arrow_RMX2061-user \
+    arrow_RMX2061-userdebug \
+    arrow_RMX2061-eng
