090c077d842118c538122a402c11122a211b3c4e
Priyanshu Singhal <priyanshusinghal0818@gmail.com>
2021-11-10 13:11:12 +0530

    RMX2061: Switch To AOSP

--- aosp_RMX2061.mk
+++ aosp_RMX2061.mk
@@ -9,22 +9,19 @@ $(call inherit-product, device/realme/RMX2061/device.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := true
 
-# Inherit some common arrow stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-
-#ArrowOS Specifics
-#
-#
+# Inherit some common stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier
-PRODUCT_NAME := arrow_RMX2061
+PRODUCT_NAME := aosp_RMX2061
 PRODUCT_DEVICE := RMX2061
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 6 Pro
 PRODUCT_MANUFACTURER := Realme
 
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
+PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="atoll-user 11 RKQ1.200903.002/ eng.root.04080114.013112 release-keys"
