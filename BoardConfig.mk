ff94b615b2d092504f077575b0e9e55446a91e91
Priyanshu Singhal <priyanshusinghal0818@gmail.com>
2021-11-30 00:54:10 +0530

    RMX2061: Force Redfin build fingerprint

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ DEVICE_PATH := device/realme/RMX2061
 TARGET_OTA_ASSERT_DEVICE := RMX2061
 
 # Build
-TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:12/SP1A.211105.003/7757856:user/release-keys
 
 # Properties
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
